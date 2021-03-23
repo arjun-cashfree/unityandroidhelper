ab5b7b3ac8d54130dfd1020beb2f89d73f998b4d
arjun-cashfree <arjun@cashfree.com>
2021-03-23 10:30:00 +0530

    closing the helper activity on receiving response

--- unityhelper/src/main/java/com/cashfree/unityhelper/CFUnityHelperActivity.java
+++ unityhelper/src/main/java/com/cashfree/unityhelper/CFUnityHelperActivity.java
@@ -66,6 +66,7 @@ public class CFUnityHelperActivity extends AppCompatActivity {
                         }
                     }
                 }
+                finish();
             }
         }
 }
